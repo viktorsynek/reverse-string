a891f15dcb79e4d677420a60e6185f4ef408462e
Viktor Synek <106328886+viktorsynek@users.noreply.github.com>
2024-02-19 16:16:17 +0100

    comments + -1 loop

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+############################################   -== INFORMATION ==-   ############################################
+
+#### THE PROGRAM WAS CREATED AND PUBLISHED BY:
+#### https://github.com/viktorsynek
+#### https://www.linkedin.com/in/viktor-synek/
+
+#############################################   -== PROGRAM ==-   ###############################################
+
+#IMPORT LIBRARIES
 import time
 
 def another():
+    # IF USER INPUTS "N" THE PROGRAM STOPS
     print("Do you want to try another? (Y/N)")
     another = input("Input: ").lower()
     
@@ -9,23 +19,22 @@ def another():
 
     start() if another == "y" else exit()
 
-def reverse_string(word):
-    lst = []
-    for i in word:
-        lst.append(i)
-    lst.reverse()
-    result = ""
-    for x in lst:
-        result += x
-    time.sleep(1)
-    print("Reversed:", result) 
-
 def start():
     print("Give me a word, I'll reverse it: ")
     time.sleep(1)
+    # THE USER GIVES THE WORD THEY WANT TO GET REVERSED
     word = input("Input: ")
     reverse_string(word)
     time.sleep(1)
     another()
+
+def reverse_string(word):
+    result = ""
+    # LOOPS THROUGH THE WORD
+    # FROM THE LAST CHARACTER TILL THE FIRST ONE
+    for i in range(len(word) - 1, -1, -1):
+        result += word[i]
+    time.sleep(1)
+    print("Reversed:", result)
 
 start()
